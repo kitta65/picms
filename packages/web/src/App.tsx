7084c7afdfcd50f5be83eff089eee888ec579f3a
kitta65 <26474260+kitta65@users.noreply.github.com>
2026-05-09 10:26:53 +0000

    wip

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,46 +1,113 @@
+import { BookOpen } from "lucide-react";
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
-import { APITester } from "./APITester";
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import {
+	NavigationMenu,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Separator } from "@/components/ui/separator";
+
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
 import "./index.css";
 
+import { columns, type Payment } from "./components/columns";
+import { DataTable } from "./components/data-table";
 import logo from "./logo.svg";
-import reactLogo from "./react.svg";
 
 export function App() {
 	return (
-		<div className="container mx-auto p-8 text-center relative z-10">
-			<div className="flex justify-center items-center gap-8 mb-8">
-				<img
-					src={logo}
-					alt="Bun Logo"
-					className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
-				/>
-				<img
-					src={reactLogo}
-					alt="React Logo"
-					className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] animate-[spin_20s_linear_infinite]"
-				/>
+		<TooltipProvider>
+			<div className="mx-6 my-2">
+				<header className="w-full flex items-center justify-left">
+					<a href="/">
+						<img src={logo} alt="logo" className="mr-4 h-8" />
+					</a>
+					<NavigationMenu>
+						<NavigationMenuList>
+							<NavigationMenuItem>
+								<NavigationMenuLink className={navigationMenuTriggerStyle()}>
+									Works
+								</NavigationMenuLink>
+							</NavigationMenuItem>
+							<NavigationMenuItem>
+								<NavigationMenuLink className={navigationMenuTriggerStyle()}>
+									Series
+								</NavigationMenuLink>
+							</NavigationMenuItem>
+							<NavigationMenuItem>
+								<NavigationMenuLink className={navigationMenuTriggerStyle()}>
+									Config
+								</NavigationMenuLink>
+							</NavigationMenuItem>
+						</NavigationMenuList>
+					</NavigationMenu>
+					<div className="ml-auto">
+						<Tooltip>
+							<TooltipTrigger asChild>
+								<Button asChild variant="outline" size="icon">
+									<a href="https://example.com">
+										<BookOpen />
+									</a>
+								</Button>
+							</TooltipTrigger>
+							<TooltipContent>
+								<p>Go to document</p>
+							</TooltipContent>
+						</Tooltip>
+					</div>
+				</header>
+				<Separator className="my-2" />
+				<div className="container mx-auto">
+					<Breadcrumb className="mb-2">
+						<BreadcrumbList>
+							<BreadcrumbItem>
+								<BreadcrumbLink href="/">Home</BreadcrumbLink>
+							</BreadcrumbItem>
+							<BreadcrumbSeparator />
+							<BreadcrumbItem>
+								<BreadcrumbLink href="/components">Components</BreadcrumbLink>
+							</BreadcrumbItem>
+							<BreadcrumbSeparator />
+							<BreadcrumbItem>
+								<BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+							</BreadcrumbItem>
+						</BreadcrumbList>
+					</Breadcrumb>
+					<DataTable
+						columns={columns}
+						data={
+							[
+								{
+									id: "728ed52f",
+									amount: 100,
+									status: "pending",
+									email: "m@example.com",
+								},
+								// ...
+							] satisfies Payment[]
+						}
+					/>
+				</div>
+				<Separator className="my-2" />
+				<footer className="mt-auto text-right text-muted-foreground">
+					Copyright © 2026 kitta65
+				</footer>
 			</div>
-			<Card>
-				<CardHeader className="gap-4">
-					<CardTitle className="text-3xl font-bold">Bun + React</CardTitle>
-					<CardDescription>
-						Edit
-						<code className="rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono">
-							src/App.tsx
-						</code>
-						and save to test HMR
-					</CardDescription>
-				</CardHeader>
-				<CardContent>
-					<APITester />
-				</CardContent>
-			</Card>
-		</div>
+		</TooltipProvider>
 	);
 }
