b4a183bd839b8a6066148986c2d3ce700c21235c
kitta65 <26474260+kitta65@users.noreply.github.com>
2026-08-07 04:32:25 +0000

    add testcases about io

--- packages/server/src/features/storage/io.test.ts
+++ packages/server/src/features/storage/io.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from "bun:test";
 import { STORAGE_POST_SCHEMA } from "./io";
 
 const VALID_INPUT = {
-	directory: "foo",
+	directory: "foo" as string,
 	id: Bun.randomUUIDv7(),
 } as const;
 
@@ -13,8 +13,20 @@ describe("STORAGE_POST_SCHEMA", () => {
 		expect(result).toStrictEqual(input);
 	});
 
-	test("fail to parse input with missing field", () => {
-		const input = {};
+	test("fail to parse input with missing field (id)", () => {
+		const input = {
+			...VALID_INPUT,
+			id: undefined,
+		} satisfies Partial<typeof VALID_INPUT>;
+		const result = STORAGE_POST_SCHEMA.safeParse(input);
+		expect(result.success).toBe(false);
+	});
+
+	test("fail to parse input with missing field (directory)", () => {
+		const input = {
+			...VALID_INPUT,
+			directory: undefined,
+		} satisfies Partial<typeof VALID_INPUT>;
 		const result = STORAGE_POST_SCHEMA.safeParse(input);
 		expect(result.success).toBe(false);
 	});
@@ -22,6 +34,15 @@ describe("STORAGE_POST_SCHEMA", () => {
 	test("fail to parse input with invalid uuid", () => {
 		const input = {
 			directory: "foo",
+			id: "bar/baz",
+		} satisfies typeof VALID_INPUT;
+		const result = STORAGE_POST_SCHEMA.safeParse(input);
+		expect(result.success).toBe(false);
+	});
+
+	test("fail to parse input with invalid directory", () => {
+		const input = {
+			directory: "", // empty string should be rejected
 			id: "bar/baz",
 		} satisfies typeof VALID_INPUT;
 		const result = STORAGE_POST_SCHEMA.safeParse(input);
