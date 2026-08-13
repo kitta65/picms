7e3498defac5e3d43bde962929b8eee0550211b9
kitta65 <26474260+kitta65@users.noreply.github.com>
2026-08-13 16:02:01 +0000

    bun's webview is now working!

--- packages/e2e/src/index.test.ts
+++ packages/e2e/src/index.test.ts
@@ -1,6 +1,22 @@
 import { describe, expect, test } from "bun:test";
 
-describe("e2e", () => {
+const { NODE_ENV, PICMS_PORT_MAIN, PICMS_PORT_WEB } = Bun.env;
+
+const URL =
+	NODE_ENV === "production"
+		? `http://localhost:${PICMS_PORT_WEB}`
+		: `http://localhost:${PICMS_PORT_MAIN}`;
+
+describe("e2e", async () => {
+	await using view = new Bun.WebView({
+		backend: {
+			type: "chrome",
+			// in this devcontainer, --no-sandbox seems required
+			argv: ["--no-sandbox"],
+		},
+	});
+	await view.navigate(URL);
+
 	test("TODO", () => {
 		expect(1 + 1).toBe(2);
 	});
