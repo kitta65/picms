29a22add190c01a6a6f25431de1f3bc1fc6833b4
kitta65 <26474260+kitta65@users.noreply.github.com>
2026-09-13 14:28:21 +0000

    refactor test helper

--- packages/web/src/shared/api.ts
+++ packages/web/src/shared/api.ts
@@ -2,7 +2,7 @@ import { type ClientResponse, hc } from "hono/client";
 import type { PicmsApi } from "picms-server/api";
 import { createContext } from "react";
 
-const DEFAULT_API_CLIENT = hc<PicmsApi>(window.location.origin);
+export const DEFAULT_API_CLIENT = hc<PicmsApi>(window.location.origin);
 export type ApiClient = typeof DEFAULT_API_CLIENT;
 
 export const ApiClientContext = createContext(DEFAULT_API_CLIENT);
