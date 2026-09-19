68121fd3c2c9852cce0101c62cf0831a30197a82
kitta65 <26474260+kitta65@users.noreply.github.com>
2026-09-19 10:25:42 +0000

    add more test cases

--- packages/server/src/infrastructures/drizzle/repositories/config-database.test.ts
+++ packages/server/src/infrastructures/drizzle/repositories/config-database.test.ts
@@ -3,65 +3,63 @@ import { DB } from "../configs";
 import { configTable } from "../tables";
 import { configDatabase } from "./config-database";
 
-describe("configDatabase", () => {
-	beforeEach(async () => {
-		await DB.delete(configTable);
+beforeEach(async () => {
+	await DB.delete(configTable);
+});
+
+describe("findFirst", () => {
+	test("retuns undefined when empty", async () => {
+		const result = await configDatabase.findFirst();
+		expect(result).toBe(undefined);
 	});
 
-	describe("findFirst", () => {
-		test("retuns undefined when empty", async () => {
-			const result = await configDatabase.findFirst();
-			expect(result).toBe(undefined);
-		});
-
-		test("return upserted value", async () => {
-			const config = {
-				timezone: "Asia/Tokyo",
-			} as const;
-			await configDatabase.upsert(config);
-			const result = await configDatabase.findFirst();
-			expect(result).toStrictEqual(config);
-		});
-
-		test("return last upserted value", async () => {
-			const config1 = {
-				timezone: "Asia/Tokyo",
-			};
-			await configDatabase.upsert(config1);
-
-			const config2 = {
-				timezone: "UTC",
-			};
-			await configDatabase.upsert(config2);
-			const result = await configDatabase.findFirst();
-
-			expect(result).toStrictEqual(config2);
-		});
+	test("return upserted value", async () => {
+		const config = {
+			timezone: "Asia/Tokyo",
+		} as const;
+		await configDatabase.upsert(config);
+		const result = await configDatabase.findFirst();
+		expect(result).toStrictEqual(config);
 	});
 
-	describe("upsert", () => {
-		test("returns upserted value", async () => {
-			const config = {
-				timezone: "Asia/Tokyo",
-			} as const;
-			const result = await configDatabase.upsert(config);
-			expect(result).toStrictEqual(config);
-		});
+	test("return last upserted value", async () => {
+		const config1 = {
+			timezone: "Asia/Tokyo",
+		};
+		await configDatabase.upsert(config1);
 
-		test("returns upserted value (null)", async () => {
-			const config = {
-				timezone: null,
-			} as const;
-			const result = await configDatabase.upsert(config);
-			expect(result).toStrictEqual(config);
-		});
+		const config2 = {
+			timezone: "UTC",
+		};
+		await configDatabase.upsert(config2);
+		const result = await configDatabase.findFirst();
 
-		test("returns default value when invalid value is inserted", async () => {
-			const config = {
-				timezone: "Invalid/TimeZone",
-			} as const;
-			const result = await configDatabase.upsert(config);
-			expect(result).toStrictEqual({ timezone: null });
-		});
+		expect(result).toStrictEqual(config2);
+	});
+});
+
+describe("upsert", () => {
+	test("returns upserted value", async () => {
+		const config = {
+			timezone: "Asia/Tokyo",
+		} as const;
+		const result = await configDatabase.upsert(config);
+		expect(result).toStrictEqual(config);
+	});
+
+	test("returns upserted value (null)", async () => {
+		const config = {
+			timezone: null,
+		} as const;
+		const result = await configDatabase.upsert(config);
+		expect(result).toStrictEqual(config);
+	});
+
+	test("returns default value when invalid value is inserted", async () => {
+		const config = {
+			timezone: "Invalid/TimeZone",
+		} as const;
+		const result = await configDatabase.upsert(config);
+		expect(result).toStrictEqual({ timezone: null });
 	});
 });
--- packages/server/src/infrastructures/drizzle/repositories/revision-database.test.ts
+++ packages/server/src/infrastructures/drizzle/repositories/revision-database.test.ts
@@ -13,114 +13,111 @@ const VALID_REVISION: Revision = {
 	createdAt: new Date(),
 };
 
-describe("revisionDatabase", () => {
-	const messageBroker = new FakeMessageBroker();
-	spyOn(messageBroker, "publish").mockImplementation((m) => m);
+const messageBroker = new FakeMessageBroker();
+spyOn(messageBroker, "publish").mockImplementation((m) => m);
 
-	const revisionDatabase = new RevisionDatabase({
-		messageBroker,
+const revisionDatabase = new RevisionDatabase({
+	messageBroker,
+});
+
+beforeEach(async () => {
+	await DB.delete(revisionTable);
+});
+
+describe("insert", () => {
+	test("returns inserted value", async () => {
+		const { data: result } = await revisionDatabase.insert(VALID_REVISION);
+		expect(result).toStrictEqual(VALID_REVISION);
 	});
 
-	beforeEach(async () => {
-		await DB.delete(revisionTable);
+	test("returns expected message", async () => {
+		const { messages: results } = await revisionDatabase.insert(VALID_REVISION);
+		expect(results.length).toBe(2);
+
+		const revisionInsertedMessage = results.find(
+			(res) => res.type === "REVISION_INSERTED",
+		);
+		expect(revisionInsertedMessage?.targetId).toBe(VALID_REVISION.id);
+
+		const revisionSignedUrlExpiredMessage = results.find(
+			(res) => res.type === "REVISION_SIGNED_URL_EXPIRED",
+		);
+		expect(revisionSignedUrlExpiredMessage?.targetId).toBe(VALID_REVISION.id);
+		expect(
+			revisionSignedUrlExpiredMessage?.createdAt.getTime(),
+		).toBeLessThanOrEqual(Date.now());
+		expect(
+			revisionSignedUrlExpiredMessage?.scheduledAt.getTime(),
+		).toBeGreaterThan(Date.now());
 	});
 
-	describe("insert", () => {
-		test("returns inserted value", async () => {
-			const { data: result } = await revisionDatabase.insert(VALID_REVISION);
-			expect(result).toStrictEqual(VALID_REVISION);
-		});
+	test("not null constraints are working", async () => {
+		// biome-ignore lint: intentional type error for test
+		const id = null as any;
+		const revision: Revision = {
+			...VALID_REVISION,
+			id,
+		};
+		await expect(revisionDatabase.insert(revision)).rejects.toThrow();
+	});
+});
 
-		test("returns expected message", async () => {
-			const { messages: results } =
-				await revisionDatabase.insert(VALID_REVISION);
-			expect(results.length).toBe(2);
-
-			const revisionInsertedMessage = results.find(
-				(res) => res.type === "REVISION_INSERTED",
-			);
-			expect(revisionInsertedMessage?.targetId).toBe(VALID_REVISION.id);
-
-			const revisionSignedUrlExpiredMessage = results.find(
-				(res) => res.type === "REVISION_SIGNED_URL_EXPIRED",
-			);
-			expect(revisionSignedUrlExpiredMessage?.targetId).toBe(VALID_REVISION.id);
-			expect(
-				revisionSignedUrlExpiredMessage?.createdAt.getTime(),
-			).toBeLessThanOrEqual(Date.now());
-			expect(
-				revisionSignedUrlExpiredMessage?.scheduledAt.getTime(),
-			).toBeGreaterThan(Date.now());
-		});
-
-		test("not null constraints are working", async () => {
-			// biome-ignore lint: intentional type error for test
-			const id = null as any;
-			const revision: Revision = {
-				...VALID_REVISION,
-				id,
-			};
-			await expect(revisionDatabase.insert(revision)).rejects.toThrow();
-		});
+describe("findById", () => {
+	test("returns unspecified when unknown uuid is specified", async () => {
+		const result = await revisionDatabase.findById(Bun.randomUUIDv7());
+		expect(result).toBe(undefined);
 	});
 
-	describe("findById", () => {
-		test("returns unspecified when unknown uuid is specified", async () => {
-			const result = await revisionDatabase.findById(Bun.randomUUIDv7());
-			expect(result).toBe(undefined);
-		});
+	test("returns specified revision", async () => {
+		const id1 = Bun.randomUUIDv7();
+		const id2 = Bun.randomUUIDv7();
 
-		test("returns specified revision", async () => {
-			const id1 = Bun.randomUUIDv7();
-			const id2 = Bun.randomUUIDv7();
+		const revision1: Revision = { ...VALID_REVISION, id: id1 };
+		const revision2: Revision = { ...VALID_REVISION, id: id2 };
 
-			const revision1: Revision = { ...VALID_REVISION, id: id1 };
-			const revision2: Revision = { ...VALID_REVISION, id: id2 };
+		await revisionDatabase.insert(revision1);
+		await revisionDatabase.insert(revision2);
 
-			await revisionDatabase.insert(revision1);
-			await revisionDatabase.insert(revision2);
+		const result1 = await revisionDatabase.findById(id1);
+		const result2 = await revisionDatabase.findById(id2);
 
-			const result1 = await revisionDatabase.findById(id1);
-			const result2 = await revisionDatabase.findById(id2);
+		expect(result1).toStrictEqual(revision1);
+		expect(result2).toStrictEqual(revision2);
+	});
+});
 
-			expect(result1).toStrictEqual(revision1);
-			expect(result2).toStrictEqual(revision2);
-		});
+describe("findByWorkId", () => {
+	test("returns all matched revisions", async () => {
+		const revision1 = { ...VALID_REVISION, id: Bun.randomUUIDv7() };
+		const revision2 = { ...VALID_REVISION, id: Bun.randomUUIDv7() };
+		await revisionDatabase.insert(revision1);
+		await revisionDatabase.insert(revision2);
+		const revisions = await revisionDatabase.findByWorkId(revision1.workId);
+		expect(revisions.length).toBe(2);
+	});
+});
+
+describe("deleteById", () => {
+	test("deleted revision is not found", async () => {
+		await revisionDatabase.insert(VALID_REVISION);
+		const resultBeforeDelete = await revisionDatabase.findById(
+			VALID_REVISION.id,
+		);
+		expect(resultBeforeDelete).toStrictEqual(VALID_REVISION);
+
+		await revisionDatabase.deleteById(VALID_REVISION.id);
+		const resultAfterDelete = await revisionDatabase.findById(
+			VALID_REVISION.id,
+		);
+		expect(resultAfterDelete).toBe(undefined);
 	});
 
-	describe("findByWorkId", () => {
-		test("returns all matched revisions", async () => {
-			const revision1 = { ...VALID_REVISION, id: Bun.randomUUIDv7() };
-			const revision2 = { ...VALID_REVISION, id: Bun.randomUUIDv7() };
-			await revisionDatabase.insert(revision1);
-			await revisionDatabase.insert(revision2);
-			const revisions = await revisionDatabase.findByWorkId(revision1.workId);
-			expect(revisions.length).toBe(2);
-		});
-	});
-
-	describe("deleteById", () => {
-		test("deleted revision is not found", async () => {
-			await revisionDatabase.insert(VALID_REVISION);
-			const resultBeforeDelete = await revisionDatabase.findById(
-				VALID_REVISION.id,
-			);
-			expect(resultBeforeDelete).toStrictEqual(VALID_REVISION);
-
-			await revisionDatabase.deleteById(VALID_REVISION.id);
-			const resultAfterDelete = await revisionDatabase.findById(
-				VALID_REVISION.id,
-			);
-			expect(resultAfterDelete).toBe(undefined);
-		});
-
-		test("does not throw when unknown id is specified", async () => {
-			try {
-				const id = Bun.randomUUIDv7();
-				await revisionDatabase.deleteById(id);
-			} catch {
-				expect.unreachable();
-			}
-		});
+	test("does not throw when unknown id is specified", async () => {
+		try {
+			const id = Bun.randomUUIDv7();
+			await revisionDatabase.deleteById(id);
+		} catch {
+			expect.unreachable();
+		}
 	});
 });
